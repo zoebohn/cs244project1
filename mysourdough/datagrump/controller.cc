ef5027c1bb3ef5bd893eea44ec63bf4b6b0bdec4
cs244 <evd2014@gmail.com>
2018-04-09 23:27:24 -0700

    aimd and delay triggered

--- mysourdough/datagrump/controller.cc
+++ mysourdough/datagrump/controller.cc
@@ -3,7 +3,15 @@
 #include "controller.hh"
 #include "timestamp.hh"
 
-#define AIMD true
+#define AIMD false 
+#define AIMD_INC 1 
+#define AIMD_DEC 0.1
+
+#define DELAY_TRIGGERED true
+#define DT_INC 1
+#define DT_DEC 10
+#define DT_THRESHOLD 350  // in milliseconds
+#define PERIOD 2 // in milliseconds, time to wait before next increase
 
 using namespace std;
 
@@ -13,7 +21,13 @@ Controller::Controller( const bool debug )
 {}
 
 /* Default: fixed window size of 100 outstanding datagrams */
-unsigned int the_window_size = 50;
+unsigned int the_window_size = 1;
+
+// last ack received, used for delay triggered
+uint64_t last_ack = 0;
+
+// last time window was updated, used for delay triggered
+uint64_t last_update = 0;
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size()
@@ -39,7 +53,7 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 
   if (AIMD) {
     if (after_timeout) {
-      the_window_size = the_window_size/2 + 1;
+      the_window_size = the_window_size*AIMD_DEC;
     }
   }
 
@@ -62,9 +76,25 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   /* Default: take no action */
 
   if (AIMD) {
-    the_window_size += 1;
+    the_window_size += AIMD_INC;
   }
 
+  if (DELAY_TRIGGERED) {
+    uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
+    if (rtt < DT_THRESHOLD && sequence_number_acked > last_ack) {
+       if (timestamp_ack_received - last_update >= PERIOD) {
+         last_update = timestamp_ack_received;
+         the_window_size += DT_INC;
+       }
+    } else {
+       last_update = timestamp_ack_received;
+       uint64_t new_window_sz = the_window_size - DT_DEC;
+       the_window_size = new_window_sz < the_window_size ? new_window_sz : 0; // check for overflow
+    }
+    cerr << "new window sz: " << the_window_size << endl;
+  }
+  last_ack = sequence_number_acked > last_ack ? sequence_number_acked : last_ack;
+  
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
 	 << " received ack for datagram " << sequence_number_acked
