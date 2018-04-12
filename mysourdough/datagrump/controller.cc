927d80b02f20003bb381c7ff4eee0537430fe569
cs244 <evd2014@gmail.com>
2018-04-12 01:23:30 -0700

    added evolving w/o brownian motion, power is 16.76

--- mysourdough/datagrump/controller.cc
+++ mysourdough/datagrump/controller.cc
@@ -17,15 +17,18 @@
 #define COOL_ALG true
 #define TICK 20 // in ms
 #define MAX_RATE 200
-#define TICKS_PER_RTT 3.5 
+#define TICKS_PER_RTT 3.5 // was 3.5 
 #define PERCENTILE_LATENCY 0.01
-#define PACKETS_PER_BUCKET 20.0
-#define EWMA_WEIGHT 0.2
+#define PACKETS_PER_BUCKET 20.0  // rename to INTERVAL_SIZE
+#define EWMA_WEIGHT 0.2 
+#define STDDEV_INC 0.0001
+
+#define DEBUG true 
 
 using namespace std;
 
 // sum of 0 through max rate * TICKS_PER_RTT
-double total_packets;
+//double total_packets;
 
 // probability distributions
 double rate_probability[MAX_RATE];
@@ -60,6 +63,14 @@ uint64_t last_tick = 0;
 
 // acks received during tick
 uint64_t packets_in_tick = 0;
+
+// acks received during previous tick
+uint64_t old_packets_in_tick = 0;
+
+// track last ackno received so don't double count packets
+uint64_t last_ackno = 0;
+
+double stddev = 0;
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size()
@@ -100,6 +111,10 @@ uint64_t factorial(uint64_t n) {
   return n*factorial(n-1);
 }
 
+double cdf(double mean, double stddev, double x) {
+  return 0.5 * erfc(-1 * (x - mean) / (stddev));
+}
+
 /* An ack was received */
 void Controller::ack_received( const uint64_t sequence_number_acked,
 			       /* what sequence number was acknowledged */
@@ -132,28 +147,44 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
        uint64_t new_window_sz = the_window_size - DT_DEC;
        the_window_size = new_window_sz < the_window_size ? new_window_sz : 0; // check for overflow
     }
-    cerr << "new window sz: " << the_window_size << endl;
+    if (DEBUG) cerr << "new window sz: " << the_window_size << endl;
   }
   last_ack = sequence_number_acked > last_ack ? sequence_number_acked : last_ack;
  
   if (COOL_ALG) {
-    packets_in_tick++;
+    if (last_ackno != sequence_number_acked) {
+      packets_in_tick++;
+      last_ackno = sequence_number_acked;
+    }
     if (timestamp_ack_received - last_tick >= TICK) {
       // Tick has elapsed.
       double sum = 0;
+      // Evolve rate probabilities.
+      stddev += STDDEV_INC;
+//      double mean = old_packets_in_tick;
+      //double val = cdf(mean, stddev, packets_in_tick + PACKETS_PER_BUCKET - old_packets_in_tick) - cdf(mean, stddev, packets_in_tick - old_packets_in_tick);
+      for (int i = 0; i < MAX_RATE; i++) {
+        double mean = i * PACKETS_PER_BUCKET;
+        double val = cdf(mean, stddev, packets_in_tick + 1.0 - old_packets_in_tick) - cdf(mean, stddev, packets_in_tick - old_packets_in_tick);
+        //double val = cdf(mean, stddev, packets_in_tick + PACKETS_PER_BUCKET - (i * PACKETS_PER_BUCKET)) - cdf(mean, stddev, packets_in_tick - (i * PACKETS_PER_BUCKET));
+        // cdf(min(ceil,new + PACKETS_PER_BUCKET) - old) - cdf(max(floor,new) - old)
+        rate_probability[i] = rate_probability[i] + val;
+        //rate_probability[i] = min(rate_probability[i], MIN_PROB) + val;
+      }
       // Update rate probabilities. 
       for (int i = 0; i < MAX_RATE; i++) {
+        // Calculating poisson
         double p = pow((i / PACKETS_PER_BUCKET) * (TICK  / 1000.0), packets_in_tick);
         p /= (double) factorial(packets_in_tick);
         p *= exp(-1 * (i / PACKETS_PER_BUCKET) * (TICK / 1000.0));
-        rate_probability[i] = /*rate_probability[i] * */p;
-        cerr << "rate_probability[" << i << "] = " << rate_probability[i] << endl;
+        rate_probability[i] = rate_probability[i] * p;
+        if (DEBUG) cerr << "rate_probability[" << i << "] = " << rate_probability[i] << endl;
         sum += rate_probability[i];
       }
       // Normalize rate probabilities. 
       for (int i = 0; i < MAX_RATE; i++) {
-        rate_probability[i] /= sum;
-        cerr << "normalized rate_probability[" << i << "] = " << rate_probability[i] << endl;
+        rate_probability[i] = rate_probability[i] / sum;
+        if (DEBUG) cerr << "normalized rate_probability[" << i << "] = " << rate_probability[i] << endl;
       }
       // Set window size based on largest rate_probability value
       sum = 0;
@@ -162,11 +193,19 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
         sum += rate_probability[i];
         i++;
       }
+      i--;
+      //uint64_t new_estimate = (i / PACKETS_PER_BUCKET) * TICKS_PER_RTT;
+      //if (new_estimate >= the_window_size) {
+      //  the_window_size = new_estimate;
+      //} else {
+      //  the_window_size = (EWMA_WEIGHT * new_estimate) + ((1 - EWMA_WEIGHT) * the_window_size);
+      //}
       the_window_size = EWMA_WEIGHT * ((i / PACKETS_PER_BUCKET) * TICKS_PER_RTT) + ((1 - EWMA_WEIGHT) * the_window_size);
-      cerr << "new window sz: " << the_window_size << endl;
+      if (DEBUG) cerr << "new window sz: " << the_window_size << endl;
       // 95th percentile just use lambda * 8 (TICK * 8 = RTT)     
       // Reset for next period.
       last_tick = timestamp_ack_received;
+      old_packets_in_tick = packets_in_tick;
       packets_in_tick = 0;
     }
   }
